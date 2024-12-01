5514c9c71ac22d3be62b0345172ecab0d8f7ddd4
Dhruv Patel <114987699+Dhruv942@users.noreply.github.com>
2024-12-01 09:09:31 +0530

    Create nonrepatingelement.cpp

---
+++ nonrepatingelement.cpp
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    // Function to find the first non-repeating character in a string.
+    char nonRepeatingChar(string &s) {
+        // Your code here
+        int n = s.length();
+
+    // Iterate over each character in the string
+    for (int i = 0; i < n; ++i) {
+        bool found = false;
+
+        // Check if the character repeats in the
+        // rest of the string
+        for (int j = 0; j < n; ++j) {
+            if (i != j && s[i] == s[j]) {
+                found = true;
+                break;
+            }
+        }
+
+        // If character does not repeat, return it
+        if (found == false) 
+            return s[i];
+    }
+
+    // If no such character is found, return '$'
+    return '$';
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int T;
+    cin >> T;
+
+    while (T--) {
+
+        string S;
+        cin >> S;
+        Solution obj;
+        char ans = obj.nonRepeatingChar(S);
+
+        if (ans != '$')
+            cout << ans;
+        else
+            cout << "-1";
+
+        cout << endl;
+
+        cout << "~"
+             << "\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
